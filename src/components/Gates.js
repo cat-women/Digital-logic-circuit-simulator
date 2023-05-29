bb9a5aa0295ae871f08cc413a151b5ef90c991a5
cat-women <imlhakpa0000@gmail.com>
2023-05-29 08:54:55 +0545

    Functional SOP design

--- src/components/Gates.js
+++ src/components/Gates.js
@@ -6,41 +6,41 @@ const createGates = (id, x = 90, y = 90) => ({
     position: { x, y }
   }),
   or: new shapes.logic.Or({
-    id: id + '-or',
+    id: id,
     position: { x, y }
   }),
   and: new shapes.logic.And({
-    id: id + '-and',
+    id: id,
     position: { x, y },
     size: { width: 100, height: 50 }
   }),
   not: new shapes.logic.Not({
-    id: id + '-not',
+    id: id,
     size: { width: 100, height: 50 },
     position: { x, y }
   }),
   nand: new shapes.logic.Nand({
-    id: id + '-nand',
+    id: id,
     size: { width: 100, height: 50 },
     position: { x, y }
   }),
   nor: new shapes.logic.Nor({
-    id: id + '-nor',
+    id: id,
     size: { width: 100, height: 50 },
     position: { x, y }
   }),
   xor: new shapes.logic.Xor({
-    id: id + '-xor',
+    id: id,
     size: { width: 100, height: 50 },
     position: { x, y }
   }),
   xnor: new shapes.logic.Xnor({
-    id: id + '-xnor',
+    id: id,
     size: { width: 100, height: 50 },
     position: { x, y }
   }),
   input: new shapes.logic.Input({
-    id: id + '-input',
+    id: id,
     position: { x, y },
     size: { width: 20, height: 20 },
     attrs: {
@@ -52,7 +52,7 @@ const createGates = (id, x = 90, y = 90) => ({
     ports: { groups: { in: { id: id } } }
   }),
   output: new shapes.logic.Output({
-    id: id + '-output',
+    id: id,
     position: { x, y },
     size: { width: 20, height: 20 },
     attrs: {
